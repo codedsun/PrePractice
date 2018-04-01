9098c1faf6336c138f8c82846fdb748a75a5b11e
Suneet Srivastava <suneetbond91@gmail.com>
2018-04-01 12:51:35 +0530

    EnumCheck

---
+++ src/EnumTest.java
@@ -0,0 +1,15 @@
+/*
+    Class for checking the enum type and it's functionality
+ */
+
+enum Human{
+    MALE, FEMALE;
+}
+public class EnumTest {
+
+    public static void main(String[] args) {
+        System.out.println(Human.MALE);
+        System.out.println(Human.FEMALE);
+        System.out.println(Human.values());
+    }
+}
