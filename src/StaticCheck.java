f0be47a44ce1d430fa73324fd2f339c29ca6fbd6
Suneet Srivastava <suneetbond91@gmail.com>
2018-04-01 11:59:31 +0530

    StaticCheck

---
+++ src/StaticCheck.java
@@ -0,0 +1,11 @@
+/*
+    You can't run the class without the main method as it throws exception MainMethod not found..
+    Remember you can compile without the main method but not run it!
+ */
+
+public class StaticCheck  {
+
+    static {
+        System.out.println("Hello, I am static block");
+    }
+}
