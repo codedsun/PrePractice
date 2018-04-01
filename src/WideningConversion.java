456e5d22b52f571fc81d6f01070994605c3ee6fe
Suneet Srivastava <suneetbond91@gmail.com>
2018-04-01 14:29:27 +0530

    WideningConversion

---
+++ src/WideningConversion.java
@@ -0,0 +1,12 @@
+/*
+
+Widening conversion example is presented in this
+ */
+
+public class WideningConversion {
+
+    public static void main(String[] args) {
+        System.out.println("S"+"U");
+        System.out.println('N'+'E'+"ET");
+    }
+}
