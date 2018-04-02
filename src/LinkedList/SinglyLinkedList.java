856c0639241098336159c13bd9ca8f853db2689b
Suneet Srivastava <suneetbond91@gmail.com>
2018-04-02 19:50:52 +0530

    SinglyLinkedList Done

---
+++ src/LinkedList/SinglyLinkedList.java
@@ -0,0 +1,85 @@
+package LinkedList;
+
+import java.util.Scanner;
+
+public class SinglyLinkedList<E> {
+
+    public static class Node<E> {
+
+        private Node<E> next;
+        private E element;
+
+        Node(E element, Node<E> next){
+            this.element = element;
+            this.next = next;
+        }
+
+        public E getElement(){
+            return element;
+        }
+        public Node<E> getNext(){
+            return next;
+        }
+        public void setNext(Node<E> next){
+            this.next = next;
+        }
+        public void setElement(E e){
+            this.element = element;
+        }
+
+    }
+
+    private Node<E> head = null;
+    private Node<E> tail = null;
+    private static int size = 0;
+
+    public boolean isEmpty(){
+        return size==0;
+    }
+
+    public E first(){
+        if(isEmpty()) return null;
+        return head.getElement();
+    }
+
+    public E last(){
+        if(isEmpty()) return null;
+        return tail.getElement();
+    }
+
+    public void addFirst(E e){
+        Node<E> newest = new Node(e, head);
+        head = newest;
+        if(size == 0)
+            tail = newest;
+        size++;
+    }
+
+    public void addLast(E e){
+        Node<E> newest = new Node(e,null);
+        if(size == 0){
+            head = tail = newest;
+        }
+        else
+            tail.setNext(newest);
+            tail = newest;
+        size++;
+    }
+
+    public void removeFirst(){
+        if(isEmpty()) return;
+        head = head.getNext();
+        size--;
+
+    }
+
+    public void printList(){
+        Node<E> node = head;
+        while(node.getNext()!=null){
+            System.out.print(node.getElement()+" ");
+            node = node.getNext();
+        }
+     }
+
+
+}
