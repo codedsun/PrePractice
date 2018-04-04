2a1bb0a1e8bd7d180db543c1627e9a8214a50ccd
Suneet Srivastava <suneetbond91@gmail.com>
2018-04-04 19:57:10 +0530

    RemoveDuplicates

---
+++ src/LeetCode/LinkedList/RemoveDuplicates.java
@@ -0,0 +1,31 @@
+package LeetCode.LinkedList;
+
+import LinkedList.SinglyLinkedList;
+import com.sun.istack.internal.NotNull;
+
+public class RemoveDuplicates {
+
+    private static SinglyLinkedList<Integer> head = new SinglyLinkedList<>();
+    public static void main(String args[]){
+        head.addFirst(1);
+        head.addLast(1);
+        head.addLast(2);
+        head.addLast(3);
+        head.addLast(3);
+        head.printList();
+        System.out.println();
+        callMe(head);
+        System.out.println();
+        System.out.println("LET'S CHECK ");
+        head.printList();
+    }
+
+   static void callMe(@NotNull  SinglyLinkedList<Integer> list){
+        SinglyLinkedList<Integer> suneet = list;
+        suneet.removeFirst();
+
+        suneet.printList();
+        list.printList();
+    }
+
+}
