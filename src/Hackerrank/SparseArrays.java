d52ce386f07d5a5868fc9e06583b69d7436f0ea9
Suneet Srivastava <suneetbond91@gmail.com>
2018-04-06 19:29:07 +0530

    SparseArrays Done

---
+++ src/Hackerrank/SparseArrays.java
@@ -0,0 +1,37 @@
+package Hackerrank;
+
+import java.util.Scanner;
+
+/*
+https://www.hackerrank.com/challenges/sparse-arrays
+ */
+
+public class SparseArrays {
+    private static String[] s;
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        s = new String[n];
+
+        for(int i=0;i<n;i++){
+            s[i] = sc.next();
+        }
+
+        int q = sc.nextInt();
+        for(int i =0;i<q;i++){
+            String query  = sc.next();
+            System.out.println(count(query));
+        }
+    }
+
+    public static int count(String query){
+        int count = 0;
+        for(int i =0;i<s.length;i++){
+            if(s[i].equalsIgnoreCase(query)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
