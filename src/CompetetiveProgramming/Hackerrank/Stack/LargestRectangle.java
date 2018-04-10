528d3285fc9260584f4c2d89d5024cf995a2a7c6
Suneet Srivastava <suneetbond91@gmail.com>
2018-04-10 20:21:49 +0530

    LargestRectangle Done

--- src/CompetetiveProgramming/Hackerrank/Stack/LargestRectangle.java
+++ src/CompetetiveProgramming/Hackerrank/Stack/LargestRectangle.java
@@ -1,10 +1,63 @@
 package CompetetiveProgramming.Hackerrank.Stack;
 
+import java.util.Scanner;
+import java.util.Stack;
+/*
+    @author : codedsun
+    Problem : https://www.hackerrank.com/challenges/largest-rectangle/problem
+ */
 public class LargestRectangle {
     public static void main(String[] args) {
-        int j =3;
-        int i = (j>2 ? 1:2);
-        System.out.println(i);
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int x ;
+      int i = 0;
+      int max_area = 0;
+      int area=0;
+      Stack<Integer> stack = new Stack<>();
+      int input[] = new int[n];
+      int j=0;
+      while(n-- >0){
+          x = sc.nextInt();
+          input[j] = x;
+          j++;
+          if(stack.isEmpty() || input[stack.peek()] <= x){
+              stack.push(i++);
+          }
+          else{
+              if(stack.isEmpty()){
+                  area = input[stack.peek()]*i;
+              }
+              else
+              {
+                  int t = input[stack.peek()];
+                  stack.pop();
+                  if(stack.isEmpty()){
+                      area = input[t]*i;
+                  }
+                  else
+                      area = input[t]*(i-stack.peek()-1);
+              }
+              if(area>max_area){
+                  max_area = area;
+              }
+          }
+      }
+      while(!stack.isEmpty()){
+          int top = stack.peek();
+          stack.pop();
+          if(stack.isEmpty()){
+              area = input[top]*i;
+          }
+          else {
+              area = input[top] * (i - stack.peek() - 1);
+          }
+          if(area>max_area){
+              max_area = area;
+          }
+
+      }
+        System.out.println(max_area);
     }
 
 }
