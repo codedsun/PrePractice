5deb340856d0367dd34c66572990729a1be0c4b4
Suneet Srivastava <suneetbond91@gmail.com>
2018-04-17 16:38:44 +0530

    Simple Language Done

---
+++ src/CompetetiveProgramming/Hackerrank/WeekOfCode37/SimpleLanguage.java
@@ -0,0 +1,37 @@
+package CompetetiveProgramming.Hackerrank.WeekOfCode37;
+
+import java.util.Scanner;
+
+public class SimpleLanguage {
+    ;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long x = 0;
+        String  s;
+        long number;
+        int n = sc.nextInt();
+        while(n-- >=1){
+
+            s = sc.next();
+
+            switch (s){
+                case "add" : number = sc.nextLong();
+                            if(x<number){
+
+                                x = number+x;
+                            }
+                            break;
+                case "set" : number = sc.nextLong();
+                            if(x<number){
+                                x = number;
+                            }break;
+            }
+
+
+
+        }
+        System.out.println(x);
+
+    }
+}
