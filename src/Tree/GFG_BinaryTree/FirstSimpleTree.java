8885f40643ef1a927a0f6c710c23390959d034d0
Suneet Srivastava <suneetbond91@gmail.com>
2018-07-19 07:40:16 +0530

    First Simple Tree Done

---
+++ src/Tree/GFG_BinaryTree/FirstSimpleTree.java
@@ -0,0 +1,21 @@
+package Tree.GFG_BinaryTree;
+
+/**
+ * @author : codedsun
+ * Created on 19/07/18
+ */
+public class FirstSimpleTree {
+
+    Node root;
+
+    FirstSimpleTree(int key) {
+        root = new Node(key);
+    }
+
+    public static void main(String[] args) {
+        FirstSimpleTree fst = new FirstSimpleTree(1);
+        fst.root.left = new Node(2);
+        fst.root.right = new Node(3);
+        fst.root.left.left = new Node(4);
+    }
+}
---
+++ src/Tree/GFG_BinaryTree/Node.java
@@ -0,0 +1,18 @@
+package Tree.GFG_BinaryTree;
+
+/**
+ * @author : codedsun
+ * Created on 19/07/18
+ */
+class Node {
+
+    int key;
+    Node left;
+    Node right;
+
+    public Node(int key) {
+        this.key = key;
+        this.left = this.right = null;
+    }
+
+}
