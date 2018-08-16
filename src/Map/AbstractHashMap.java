5cb18af36a5da25757f6feacdad2455db0c79659
Suneet Srivastava <suneetbond91@gmail.com>
2018-08-16 23:25:44 +0530

    Abstract Hash Map

---
+++ src/Map/AbstractHashMap.java
@@ -0,0 +1,13 @@
+package Map;
+
+/**
+ * @author : codedsun
+ * Created on 13/08/18
+ */
+
+/**
+ * Implementation of Hash Table
+ */
+public abstract class AbstractHashMap<K, V> extends AbstractMap<K,V> {
+
+}
