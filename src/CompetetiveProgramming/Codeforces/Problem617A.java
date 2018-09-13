69c75788d8c1f4d679ea134498cf4446d3c8e26f
Suneet Srivastava <suneetbond91@gmail.com>
2018-09-14 00:39:32 +0530

    Problem617A

---
+++ src/CompetetiveProgramming/Codeforces/Problem617A.java
@@ -0,0 +1,18 @@
+package CompetetiveProgramming.Codeforces;
+
+/**
+ * @author : codedsun
+ * Created on 14/09/18
+ */
+
+import java.util.Scanner;
+
+/**
+ * <a href="http://codeforces.com/problemset/problem/617/A"/>
+ */
+public class Problem617A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
