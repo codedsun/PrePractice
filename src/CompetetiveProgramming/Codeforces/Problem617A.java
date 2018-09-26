74cb0c4b2806af37e5eea43784e5fd6ef111af33
Suneet Srivastava <suneetbond91@gmail.com>
2018-09-26 21:54:04 +0530

    Refactor

--- src/CompetetiveProgramming/Codeforces/Problem617A.java
+++ src/CompetetiveProgramming/Codeforces/Problem617A.java
@@ -13,6 +13,19 @@ import java.util.Scanner;
 public class Problem617A {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int a[] = new int[]{5,4,3,2,1};
+        int n = sc.nextInt();
+        int c = 0;
+        for(int i = 0; i<5; i++) {
+            if(n >= a[i]) {
+                c++;
+                n = n - a[i];
+                i = 0;
+            }
+        }
+
+        System.out.println(c);
+
 
     }
 }
